820cd74406b56cd8c2725a11018fdf80f9124fc6
jr <72330366+roetske@users.noreply.github.com>
2024-06-30 00:56:46 +0200

    Update espnow_v4_module2.ino

--- mod2/espnow_v4_module2.ino
+++ mod2/espnow_v4_module2.ino
@@ -104,7 +104,7 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
     }
   else
    { Serial.println("send Oeps:(");
-     bsucces = false;//gebruiken voor safety zenden
+     bsucces = false;//to do process this variable?
    }
 }
 
